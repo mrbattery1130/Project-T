3bd1228bd0e3ded4cc9045015dd0a2d7cf281009
xintong.wu <Arctic1130>
2022-01-07 17:54:32 +0800

    add catalogue api

--- app/api/v1/icon_manager.py
+++ app/api/v1/icon_manager.py
@@ -1,15 +1,12 @@
-from typing import List
-
-from flask.json import jsonify
 from lin.apidoc import api, DocResponse
 from lin.redprint import Redprint
 
 from app.exception.api import CatalogueNotFound
-from app.model.icon_manager.app_model import App
 from app.model.icon_manager.catalogue_model import Catalogue
-from app.validator.schema import CatalogueOutSchema, BookSchemaList, CatalogueSchemaList
+from app.validator.schema import CatalogueOutSchema, CatalogueSchemaList
 
 app_icon_api = Redprint('app_icon')
+
 
 @app_icon_api.route('/catalogue/<int:id>')
 @api.validate(
