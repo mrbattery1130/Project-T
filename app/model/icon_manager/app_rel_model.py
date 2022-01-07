d292c4e3e03144bf70d3bc877d21ffbf299fc547
mrbattery1130 <33189865+mrbattery1130@users.noreply.github.com>
2022-01-07 23:41:03 +0800

    修改启动类名可为空

--- app/model/icon_manager/app_rel_model.py
+++ app/model/icon_manager/app_rel_model.py
@@ -5,5 +5,5 @@ from sqlalchemy import Column, String, Integer
 class AppRel(Base):
     id: Column = Column(Integer, primary_key=True, autoincrement=True)
     package_name: Column = Column(String(100), nullable=False)
-    launch_name: Column = Column(String(500), nullable=False)
+    launch_name: Column = Column(String(500), nullable=True)
     app_id: Column = Column(Integer, autoincrement=True)
